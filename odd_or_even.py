7884c38cef0284417789c48b5139b4f3615d6dce
olafoyeku-josiah <olafoyekujosiah@gmail.com>
2025-08-06 22:37:23 -0700

    removed duplicate files

--- odd_or_even.py
+++
@@ -1,9 +0,0 @@
-print("a program to check if a number is odd or even")
-#getting input from user
-num=int(input("enter a number: "))
-
-#check if number is odd or even
-if num % 2==0:
-    print("{} is even. ".format(num))
-else:
-    print("{} is odd. ".format(num))
