6ff8df4e28c6864111d6d85474897f14c6e85377
olafoyeku-josiah <olafoyekujosiah@gmail.com>
2025-08-06 23:57:31 -0700

    exercise files

---
+++ DAY-FOUR_exercises/number_count.py
@@ -0,0 +1,14 @@
+def count_words_in_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            content=file.read()
+            words=content.split()
+            word_count=len(words)
+            print("total number of words is : {}".format(word_count))
+    except FileNotFoundError:
+        print("The file can not be found.")
+    except Exception as e:
+        print("an error occured {}".format(e))
+
+
+count_words_in_file("realtor_data_processing.txt")
