7a6c8c83a073d6a3342150550b7232c9bcd4cdc2
olafoyeku-josiah <olafoyekujosiah@gmail.com>
2025-08-08 11:26:34 -0700

    new files to be added

---
+++ DAY-FOUR_exercises/random.py
@@ -0,0 +1,6 @@
+from utilis import is_even
+
+for num in range(1,11):
+    if is_even(num):
+        print("{} is even".format(num))
+    
---
+++ DAY-FOUR_exercises/utilis.py
@@ -0,0 +1,3 @@
+def is_even(number):
+    """a module to check whether a given number is even """
+    return number % 2 == 0
